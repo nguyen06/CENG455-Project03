b34f4c27509e6ff67ebf351bb9afaab0939a56bc
nguyen06 <nguyen06@users.noreply.github.com>
2017-03-13 15:44:22 -0700

    Update access_functions.h

--- Sources/access_functions.h
+++ Sources/access_functions.h
@@ -10,26 +10,23 @@
 #include "os_tasks.h"
 #include "global_variables.h"
 
+
 #ifndef SOURCES_ACCESS_FUNCTIONS_H_
 #define SOURCES_ACCESS_FUNCTIONS_H_
 
-_task_id dd_tcreate(uint_32 template_index, uint_32 deadline);
-uint_32 dd_delete(uint_32 task_id);
-uint_32 dd_return_active_list(task_list **list);
-uint_32 dd_return_overdue_list(task_list **list);
+_task_id dd_tcreate(uint32_t template_index, uint32_t deadline, uint32_t execution_time);
+uint32_t dd_delete(uint32_t task_id, uint32_t creation_time);
+uint32_t dd_return_active_list(task_list **list);
+uint32_t dd_return_overdue_list(task_list **list);
 void dd_init();
+void delay(int val);
+int generateRandom(int l, int u);
 
 /*linked list functions*/
 bool isEmpty(task_list *head);
-int length(task_list *head);
+int length(task_list** head);
 void displayForward(task_list *head);
-void displayBackward(task_list *head);
-void insertFirst(task_list *head, task_list *tail, uint_32 tid, uint_32 deadline, uint_32 creation_time);
-void insertLast(task_list *tail, uint_32 tid, uint_32 deadline, uint_32 creation_time);
-task_list* deleteFirst(task_list *head, task_list *last);
-task_list* deleteLast(task_list *head, task_list *last);
-task_list* delete(task_list* head, uint_32 tid, task_list *last);
-insertAfter(task_list* head, uint_32 tid, task_list *last, uint_32 newtid, uint_32 deadline, uint_32 creation_time);
-
+void insert(task_list** head, task_list** last, uint32_t tid, uint32_t deadline, uint32_t creation_time);
+void delete(task_list** head, task_list** last, uint32_t tid,uint32_t creation_time);
 
 #endif /* SOURCES_ACCESS_FUNCTIONS_H_ */
