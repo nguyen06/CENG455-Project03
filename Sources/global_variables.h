c61960a9a11d92b14e20ca68f1caaf77a891a14d
nguyen06 <nguyen06@users.noreply.github.com>
2017-03-13 15:44:58 -0700

    Update global_variables.h

--- Sources/global_variables.h
+++ Sources/global_variables.h
@@ -11,7 +11,7 @@
 #ifndef SOURCES_GLOBAL_VARIABLES_H_
 #define SOURCES_GLOBAL_VARIABLES_H_
 
-#define NUM_USER_TASKS 4
+#define NUM_USER_TASKS 10
 
 //Task IDs
 #define DD_TASK 5
@@ -20,21 +20,22 @@
 //Queue IDs
 #define DD_QUEUE 7
 #define CREATOR_QUEUE 8
-#define USER1_QUEUE 9
-#define PUTLINE_QUEUE 10
+#define TCREAT_QUEUE 9
+#define DELETE_QUEUE 10
 #define USER2_QUEUE 11
 #define USER3_QUEUE 12
 #define USER4_QUEUE 13
 
-_queue_id handler_qid;
-_queue_id putline_qid;
+_queue_id dd_qid;
+_queue_id tcreate_qid;
+_queue_id delete_qid;
 
 char output_buf[200];
 char putline_buf[200];
 char getline_buf[200];
 int counter;// = 11;
 char Cr = 0x0d;
-int j;
+//int srand_;
 
 
 
@@ -42,20 +43,35 @@ int j;
 //message structure
 typedef struct{
 	MESSAGE_HEADER_STRUCT HEADER;
-	unsigned char DATA[200];
+	_task_id tid;
+	int deadline;
+	int creation_time;
 }MESSAGE, *MSG_PTR;
 
 
 //task structure
-typedef struct{
-	uint_32 tid;
-	uint_32 deadline;
-	uint_32 creation_time;
-	struct task_list *next_cell;
-	struct task_list *previous_cell;
-}task_list, task_list;
+typedef struct t_list{
+	uint32_t tid;
+	uint32_t deadline;
+	uint32_t creation_time;
+	struct t_list *next_cell;
+	struct t_list *previous_cell;
+}task_list;
 
 MSG_PTR dd_msg_ptr;
+TIME_STRUCT start_t;
+
+int is_running = 0;
+int tt = 0;
+bool interrupt_occurs = false;
+
+/*arrival time of aperiodic task*/
+//int arrive_t = 0;
+
+_pool_id msg_pool;
+
 
 
 #endif /* SOURCES_GLOBAL_VARIABLES_H_ */
+
+
