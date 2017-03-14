0f207474278eb97c97dc87484fd5a18877251157
nguyen06 <nguyen06@users.noreply.github.com>
2017-03-13 20:22:00 -0700

    Update access_functions.h

--- Sources/access_functions.h
+++ Sources/access_functions.h
@@ -14,7 +14,7 @@
 #ifndef SOURCES_ACCESS_FUNCTIONS_H_
 #define SOURCES_ACCESS_FUNCTIONS_H_
 
-_task_id dd_tcreate(uint32_t template_index, uint32_t deadline, uint32_t execution_time);
+_task_id dd_tcreate(uint32_t template_index, uint32_t deadline);
 uint32_t dd_delete(uint32_t task_id, uint32_t creation_time);
 uint32_t dd_return_active_list(task_list **list);
 uint32_t dd_return_overdue_list(task_list **list);
@@ -28,5 +28,6 @@ int length(task_list** head);
 void displayForward(task_list *head);
 void insert(task_list** head, task_list** last, uint32_t tid, uint32_t deadline, uint32_t creation_time);
 void delete(task_list** head, task_list** last, uint32_t tid,uint32_t creation_time);
+void schedule_task(_task_id tid);
 
 #endif /* SOURCES_ACCESS_FUNCTIONS_H_ */
